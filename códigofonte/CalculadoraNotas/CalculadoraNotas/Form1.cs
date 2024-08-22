d7a28bba85cc4af9bace28c51c9aedecda688bff
salatielmarinho <salatielmarinho@gmail.com>
2024-08-22 17:03:51 -0300

    Aula 3

---
+++ códigofonte/CalculadoraNotas/CalculadoraNotas/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculadoraNotas
+{
+    public partial class frmCalculadoraNota : Form
+    {
+        public frmCalculadoraNota()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNota1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void txtNota2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void txtSub_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+                e.Handled = true;
+        }
+    }
+}
